5f75c03d06c9b7f74cc1121ff65b832b4aa77612
sanjay <sanjay.r@testyantra.com>
2023-05-30 10:16:54 +0530

    Create Project

---
+++ Actitime/src/com/actitime/testscript/ProjectModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ProjectModule {
+
+	@Test
+	public void testCreateProject() {
+		Reporter.log("Create Project",true);
+	}
+	
+}
