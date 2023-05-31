f672c50ba94115e2630cd9298450fec334d1fce4
seleniumbysandeep2 <130442957+seleniumbysandeep2@users.noreply.github.com>
2023-05-31 08:49:10 +0530

    Create TaskModule.java

---
+++ Actitime/src/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+
+	@Test
+	public void testCreateTask() {
+		Reporter.log("Create Task",true);
+	}
+	
+}
